e050c200bf9e5e16384b4bb73498eb98a12f32e2
Timur Bakaev <ttbakaeff@gmail.com>
2021-08-23 16:22:47 +0300

    add slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,35 @@
 import React from "react";
 import {Button, Card, Col, Container, Image, Media, Row} from "react-bootstrap";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 import blog1 from "../assets/images/blog-1.jpg";
 import blog2 from "../assets/images/blog-2.jpg";
 import blog3 from "../assets/images/blog-3.jpg";
 import blog4 from "../assets/images/blog-4.jpg";
-import blog5 from "../assets/images/blog-5.jpg";
-import blog6 from "../assets/images/blog-6.jpg";
-import blog7 from "../assets/images/blog-7.jpg";
-import blog8 from "../assets/images/blog-8.jpg";
+import recent1 from "../assets/images/recent-1.jpg";
+import recent2 from "../assets/images/recent-2.jpg";
+import recent3 from "../assets/images/recent-3.jpg";
+import recent4 from "../assets/images/recent-4.jpg";
 import insta1 from "../assets/images/insta-1.jpg";
 import insta2 from "../assets/images/insta-2.jpg";
 import insta3 from "../assets/images/insta-3.jpg";
 import insta4 from "../assets/images/insta-4.jpg";
 import insta5 from "../assets/images/insta-5.jpg";
 import insta6 from "../assets/images/insta-6.jpg";
+import slide1 from "../assets/images/slide1.jpg";
+import slide2 from "../assets/images/slide2.jpg";
+import slide3 from "../assets/images/slide3.jpg";
+import slide4 from "../assets/images/slide4.jpg";
+import slide5 from "../assets/images/slide5.jpg";
+import slide6 from "../assets/images/slide6.jpg";
+import slide7 from "../assets/images/slide7.jpg";
+import slide8 from "../assets/images/slide8.jpg";
 
 class Home extends React.Component {
     render() {
         return (
-            <Container className="home-container">
+            <div>
+                <Container className="home-container">
                 <Row>
                     <Col lg={8} md={12}>
                         <Card className="mb-5">
@@ -96,28 +107,28 @@ class Home extends React.Component {
                             <h5>Recent Posts</h5>
                             <div>
                                 <Media className="mb-2 recent-media">
-                                    <img src={blog6} className="mr-3" alt=""/>
+                                    <img src={recent1} className="mr-3" alt=""/>
                                     <Media.Body>
                                         <a href={"she-is-known-for-playing"}>She is known for playing</a>
                                         <p>20 Mar, 2020</p>
                                     </Media.Body>
                                 </Media>
                                 <Media className="mb-2 recent-media">
-                                    <img src={blog7} className="mr-3" alt=""/>
+                                    <img src={recent2} className="mr-3" alt=""/>
                                     <Media.Body>
                                         <a href={"craft-a-life-you-love"}>Craft a life you love</a>
                                         <p className="font-weight-light">12 Mar, 2020</p>
                                     </Media.Body>
                                 </Media>
                                 <Media className="mb-2 recent-media">
-                                    <img src={blog8} className="mr-3" alt=""/>
+                                    <img src={recent3} className="mr-3" alt=""/>
                                     <Media.Body>
                                         <a href={"wonderland-of-ice-place"}>Wonderland of ice place</a>
                                         <p className="font-weight-light">17 Feb, 2020</p>
                                     </Media.Body>
                                 </Media>
                                 <Media className="mb-2 recent-media">
-                                    <img src={blog5} className="mr-3" alt=""/>
+                                    <img src={recent4} className="mr-3" alt=""/>
                                     <Media.Body>
                                         <a href={"publish-your-passions"}>Publish your passions</a>
                                         <p className="font-weight-light">12 Jan, 2020</p>
@@ -202,6 +213,62 @@ class Home extends React.Component {
                     </Col>
                 </Row>
             </Container>
+                <Splide
+                    options={ {
+                        type: 'loop',
+                        autoplay: true,
+                        height: '12rem',
+                        perPage: 8,
+                        cover: true,
+                        perMove: 1,
+                        pagination: false,
+                        arrows: false,
+                        breakpoints: {
+                            1400: {
+                                perPage: 7,
+                                height: '10rem',
+                            },
+                            1000: {
+                                perPage: 6,
+                                height: '10rem',
+                            },
+                            800: {
+                                perPage: 4,
+                                height: '9rem',
+                            },
+                            540: {
+                                perPage: 3,
+                                height: '9rem',
+                            }
+                        }
+                    } }
+                >
+                    <SplideSlide>
+                        <img src={slide1} alt=""/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src={slide2} alt=""/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src={slide3} alt=""/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src={slide4} alt=""/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src={slide5} alt=""/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src={slide6} alt=""/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src={slide7} alt=""/>
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src={slide8} alt=""/>
+                    </SplideSlide>
+                </Splide>
+            </div>
         )
     }
 }
